cffc86130dafcd1ef655ce33f430ca8c67a3c9f2
WXLingrammer <lingwenxing_@hotmail.com>
2022-04-10 13:36:10 +0800

    Wk5-Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h2>Today is {{day}}</h2>
+  <h2> {{a+b}}</h2>
+  <h2> {{"Akhil "+"Banagiri"}}</h2>
+  <h2> {{day.length}}</h2>
+  <h2> {{day.toUpperCase()}}</h2>
+  <h1> {{disp()}}</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public day = "Tuesday";
+  public a = 10;
+  public b = 20;
   title = 'Test';
+  disp(){
+    return `Today is ${this.day}`;
+  }
 }
